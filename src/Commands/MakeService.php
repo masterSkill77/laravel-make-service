7e99ba83065752a0e2bd11c6dfa2236cea1e7d7d
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-10-16 08:22:06 +0300

    feat : update namespace and folder

--- src/Commands/MakeService.php
+++ src/Commands/MakeService.php
@@ -3,6 +3,7 @@
 namespace Masterskill\ServicePackage\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 
 class MakeService extends Command
 {
@@ -27,50 +28,36 @@ class MakeService extends Command
     {
         $filename = $this->argument('name');
 
-        $directory = config('service-package.service_directory');
-        $defaultNamespace = config('service-package.namespace');
+        $directory = Config::get('service-package.service_directory');
+        $defaultNamespace = Config::get('service-package.namespace');
 
         // Separate the class name and namespace path if provided in the format "Namespace/ClassName."
         $segments = explode('/', $filename);
         $className = end($segments);
         $namespace = count($segments) > 1 ? implode('\\', array_slice($segments, 0, -1)) : $defaultNamespace;
-        $folder = count($segments) > 1 ? implode('\\', array_slice($segments, 0, -1)) : DIRECTORY_SEPARATOR;
+        $folder = count($segments) > 1 ? implode(DIRECTORY_SEPARATOR, array_slice($segments, 0, -1)) : '';
 
-        $directoryPath = $directory . str_replace('\\', DIRECTORY_SEPARATOR, $folder);
+        $directoryPath = ($folder == '') ? $directory : $directory . DIRECTORY_SEPARATOR . $folder;
 
         // Create the directory if necessary.
         if (!is_dir($directoryPath)) {
             mkdir($directoryPath, 0755, true);
         }
         if (!str_starts_with($namespace, $defaultNamespace)) {
-            $namespace = $defaultNamespace . $namespace;
+            $namespace = $defaultNamespace . '\\' . $namespace;
         }
 
         // Generate the complete file path using the namespace and class name.
         $filePath = $directoryPath . DIRECTORY_SEPARATOR . $className . '.php';
 
-        $classContent = <<<'PHP'
-            <?php
+        // Generate the content of the class file.
+        $classContent = "<?php\n\nnamespace $namespace;\n\nclass $className\n{\n    public function __construct()\n    {\n        // Constructor of the class\n    }\n}\n";
 
-            namespace {{Namespace}};
-
-            class {{ClassName}}
-            {
-                public function __construct()
-                {
-                    // Constructor of the class
-                }
-            }
-            PHP;
-
-        $classContent = str_replace('{{Namespace}}', $namespace, $classContent);
-        $classContent = str_replace('{{ClassName}}', $className, $classContent);
-
-        if (file_put_contents($className, $classContent)) {
+        if (file_put_contents($filePath, $classContent)) {
             $this->info("The service $filePath has been created successfully.");
             $this->info("Enjoy your development :)");
         } else {
-            $this->error("Une erreur est survenue lors de la création du service.");
+            $this->error("An error occurred while creating the service.");
         }
     }
 }
